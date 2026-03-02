9aba9bf6d8730ba7cced8c89b2e68ceb96541576
itsservice <it_service@greyhound.co.th>
2026-03-02 18:17:30 +0700

    Update lineService.js

--- lineService.js
+++ lineService.js
@@ -1,12 +1,17 @@
 const axios = require('axios');
+const { LINE_CHANNEL_ACCESS_TOKEN } = require('./env');
 const { lineHeaders } = require('./line');
 
-// ================= LINE PUSH FLEX =================
-const linePushFlex = (to, flexMessage) =>
-  axios.post(
+const linePushFlex = (to, flexMessage) => {
+  if (!LINE_CHANNEL_ACCESS_TOKEN) {
+    throw new Error('Missing env: LINE_CHANNEL_ACCESS_TOKEN');
+  }
+
+  return axios.post(
     'https://api.line.me/v2/bot/message/push',
     { to, messages: [flexMessage] },
-    { headers: lineHeaders }
+    { headers: lineHeaders, timeout: 15000 }
   );
+};
 
 module.exports = { linePushFlex };
