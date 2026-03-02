f318193e2a7c91fa752de6f3a70945d08b746a89
itsservice <it_service@greyhound.co.th>
2026-03-02 16:36:52 +0700

    env.js

--- env.js
+++ env.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
-const LINE_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || '';
-const LARK_ENCRYPT_KEY = process.env.LARK_ENCRYPT_KEY || '';
-
 module.exports = {
-  PORT,
-  LINE_TOKEN,
-  LARK_ENCRYPT_KEY
+  PORT: process.env.PORT || 3000,
+  LINE_TOKEN: process.env.LINE_CHANNEL_ACCESS_TOKEN || '',
+  LARK_ENCRYPT_KEY: process.env.LARK_ENCRYPT_KEY || ''
 };
