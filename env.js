ca129e78b63bd29d133b8325770a26594af7a646
itsservice <it_service@greyhound.co.th>
2026-03-02 17:39:39 +0700

    Update env.js

--- env.js
+++ env.js
@@ -1,16 +1,8 @@
-LINE_CHANNEL_ACCESS_TOKEN=YOUR_LINE_TOKEN
-LINE_CHANNEL_SECRET=YOUR_LINE_SECRET
-
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
-const LINE_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN || '';
-const LINE_SECRET = process.env.LINE_CHANNEL_SECRET || '';
-const LARK_ENCRYPT_KEY = process.env.LARK_ENCRYPT_KEY || '';
-
 module.exports = {
-  PORT,
-  LINE_TOKEN,
-  LINE_SECRET,
-  LARK_ENCRYPT_KEY
+  PORT: process.env.PORT || 3000,
+  LINE_CHANNEL_ACCESS_TOKEN: process.env.LINE_CHANNEL_ACCESS_TOKEN || '',
+  LINE_CHANNEL_SECRET: process.env.LINE_CHANNEL_SECRET || '',
+  LARK_ENCRYPT_KEY: process.env.LARK_ENCRYPT_KEY || ''
 };
