5832d86927209e8501e4b7b85e9dc79d6459b384
itsservice <it_service@greyhound.co.th>
2026-03-02 17:57:47 +0700

    Update lineLookup.js

--- lineLookup.js
+++ lineLookup.js
@@ -1,21 +1,6 @@
 const axios = require('axios');
-const { LINE_TOKEN } = require('../config/env');
+const { LINE_CHANNEL_ACCESS_TOKEN } = require('./env');
 
 const headers = {
-  Authorization: `Bearer ${LINE_TOKEN}`
+  Authorization: `Bearer ${LINE_CHANNEL_ACCESS_TOKEN}`
 };
-
-async function getUserProfileInGroup(groupId, userId) {
-  // displayName จะได้ก็ต่อเมื่อ user ยังเป็นสมาชิก และ bot อยู่ใน group
-  const url = `https://api.line.me/v2/bot/group/${groupId}/member/${userId}`;
-  const res = await axios.get(url, { headers, timeout: 15000 });
-  return res.data; // { displayName, userId, pictureUrl, statusMessage }
-}
-
-async function getGroupSummary(groupId) {
-  const url = `https://api.line.me/v2/bot/group/${groupId}/summary`;
-  const res = await axios.get(url, { headers, timeout: 15000 });
-  return res.data; // { groupId, groupName, pictureUrl }
-}
-
-module.exports = { getUserProfileInGroup, getGroupSummary };
