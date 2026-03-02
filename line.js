b0966dd06f696086dd2c90830722861d2f37e1d7
itsservice <it_service@greyhound.co.th>
2026-03-02 16:37:34 +0700

    Update line.js

--- line.js
+++ line.js
@@ -1,8 +1,8 @@
 const { LINE_TOKEN } = require('./env');
 
-const lineHeaders = {
-  Authorization: `Bearer ${LINE_TOKEN}`,
-  'Content-Type': 'application/json'
+module.exports = {
+  lineHeaders: {
+    Authorization: `Bearer ${LINE_TOKEN}`,
+    'Content-Type': 'application/json'
+  }
 };
-
-module.exports = { lineHeaders };
